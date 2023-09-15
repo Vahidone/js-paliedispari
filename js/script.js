91573848d4f5407550120ebb20d219cb0a49caf8
Vahidone <v.ban.roma@gmail.com>
2023-09-15 17:40:41 +0200

    feat: add js function for exercise (pari e dispari)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+
+// // **********************************************************************************************
+// ESERCIZIO N2 ==>>>   PALINDROMA   <<<==
+// *************************************************************************************************
+
+
+
+
 const inputWord = document.querySelector('.input-word');
 const btnVerify = document.getElementById('button-addon2');
 const btnReset = document.querySelector('.btn-reset');
@@ -8,7 +16,7 @@ const printPlace = document.getElementById('print-result');
 function verifyWord () {
 
   const userWord = inputWord.value;
-  
+
   //  punto 3
   const reversedWord = userWord.split('').reverse().join('');
 
@@ -36,6 +44,44 @@ btnReset.addEventListener('click', function(){
 
 
 
+// // **********************************************************************************************
+// ESERCIZIO N2 ==>>>   PARI E DISPARI   <<<==
+// *************************************************************************************************
 
+// punto 2
+const rispostaPc = document.querySelector('.risposta-pc');
+const stampaRisultato = document.getElementById('stampa-risultato');
+const bottonVerifica = document.getElementById('button-verifica');
+const bottoneReset = document.querySelector('.btn-annulla');
 
+function isPari(numero) {
+  return numero % 2 === 0;
+}
+// punto 3
+bottonVerifica.addEventListener('click', function () {
+  const inputWord = document.querySelector('.input-word').value; 
+  const inputNumber = parseInt(document.querySelector('.input-number').value);
+// punto 4 
+  if (isNaN(inputNumber) || inputNumber < 1 || inputNumber > 5) {
+    rispostaPc.textContent = 'Inserisci un numero da 1 a 5.';
+  } else {
+    const numeroCasuale = Math.floor(Math.random() * 5) + 1;
+    const somma = inputNumber + numeroCasuale;
+    const risultato = isPari(somma) ? 'pari' : 'dispari';
 
+    rispostaPc.textContent = `Il computer ha scelto ${numeroCasuale}. La somma del numero scelto da te e quello generato dal pc è ${somma}, che è un numero ${risultato}.`;
+// punto 5
+    if (inputWord === 'pari' && risultato === 'pari') { 
+      stampaRisultato.textContent = 'Hai vinto!';
+    } else {
+      stampaRisultato.textContent = 'Hai perso!';
+    }
+  }
+});
+// punto 6
+bottoneReset.addEventListener('click', function () {
+  document.querySelector('.input-word').value = '';
+  document.querySelector('.input-number').value = '';
+  document.querySelector('.risposta-pc').textContent = '';
+  document.getElementById('stampa-risultato').textContent = '';
+});
